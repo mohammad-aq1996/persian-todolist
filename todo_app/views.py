38a80d22be919cf9ba72104811f3a62e18606b1a
Mohammad <mohammad.aq1996@outlook.com>
2022-03-12 22:44:48 +0330

    add comment to views

--- todo_app/views.py
+++ todo_app/views.py
@@ -6,17 +6,26 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 class SignUpView(CreateView):
+    """
+        Signup view with the use of Django built-in signup view
+        After signing up successfully user will automatically redirect to the login page
+    """
     form_class = UserCreateForm
     success_url = reverse_lazy('todo_app:login')
     template_name = 'signup.html'
 
 
 class ToDoCreate(LoginRequiredMixin, CreateView):
+    """
+        Login is required for this view, if the user isn't logged in he/she will automatically redirect to the login page
+        Creating new to-do list with these options: 'title', 'content', 'important', and 'completed'
+        After adding successfully new to-do list user will automatically redirect to the relevant detail page
+        I initial form user field to current User to specify each to-do list belongs to which one
+    """
     login_url = '/login/'
     redirect_field_name = 'next'
 
     form_class = ToDoForm
-    # success_url = reverse_lazy('todo_app:detail')
     template_name = 'todo_form.html'
 
     def get_initial(self):
@@ -25,7 +34,15 @@ class ToDoCreate(LoginRequiredMixin, CreateView):
         return initial
 
 
-class ToDoListView(ListView):
+class ToDoListView(LoginRequiredMixin, ListView):
+    """
+       Login is required for this view, if the user isn't logged in he/she will automatically redirect to the login page
+       Returns number and list of to-do list's users that isn't completed
+       On every page user will see 2 lists of to-do list
+   """
+    login_url = '/login/'
+    redirect_field_name = 'next'
+
     model = ToDo
     template_name = 'todolist.html'
     context_object_name = 'todo_list'
@@ -41,6 +58,10 @@ class ToDoListView(ListView):
 
 
 class ToDoCompletedList(ToDoListView):
+    """
+        This class inherit from ToDoListView
+        Returns number and list of to-do list's users that completed
+    """
 
     def get_queryset(self):
         return self.model.objects.filter(user__username=self.request.user, completed__isnull=False).order_by('-completed')
@@ -53,6 +74,10 @@ class ToDoCompletedList(ToDoListView):
 
 
 class ToDoDetailView(LoginRequiredMixin, DetailView):
+    """
+       Login is required for this view, if user isn't logged in he/she will automatically redirect to login page
+       Returns detail of a specific to do list
+   """
     login_url = '/login/'
     redirect_field_name = 'next'
 
@@ -62,6 +87,11 @@ class ToDoDetailView(LoginRequiredMixin, DetailView):
 
 
 class ToDoUpdateView(LoginRequiredMixin, UpdateView):
+    """
+       Login is required for this view, if user isn't logged in he/she will automatically redirect to login page
+       With this class users can update their to-do lists and save them.
+       This class uses the same template as creating a to-do list
+   """
     login_url = '/login/'
     redirect_field_name = 'next'
 
@@ -71,6 +101,11 @@ class ToDoUpdateView(LoginRequiredMixin, UpdateView):
 
 
 class ToDoDeleteView(LoginRequiredMixin, DeleteView):
+    """
+        Login is required for this view, if user isn't logged in he/she will automatically redirect to login page
+        With this class users can delete their to do lists.
+
+    """
     login_url = '/login/'
     redirect_field_name = 'next'
 
