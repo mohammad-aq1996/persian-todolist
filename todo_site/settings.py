99b7e77078011eca315edebec1cc4db3f6df8936
Mohammad Ali Gholampour <mohammad.aq1996@yahoo.com>
2022-04-24 00:28:05 +0430

    add gmail to settings

--- todo_site/settings.py
+++ todo_site/settings.py
@@ -221,4 +221,10 @@ AUTH_USER_MODEL = 'account_app.User'
 
 # Email
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'mamalidjango@gmail.com'
+EMAIL_HOST_PASSWORD = 'M7410852m'
