b6b76e7fe67739350abc89005c53b3931a872751
Mohammad Ali Gholampour <mohammad.aq1996@yahoo.com>
2022-04-23 23:39:33 +0430

    add password change view

--- account_app/urls.py
+++ account_app/urls.py
@@ -7,4 +7,14 @@ urlpatterns = [
     path('login/', auth_view.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_view.LogoutView.as_view(), name='logout'),
     path('signup/', views.SignUpView.as_view(), name='signup'),
+
+    path('password/change/',
+         views.PassChangeView.as_view(template_name='passChange.html'),
+         name='password_change'
+         ),
+    path('password/change/done/',
+         auth_view.PasswordChangeDoneView.as_view(template_name='passChaneDone.html'),
+         name='password_change_done'
+         ),
+
 ]
--- account_app/views.py
+++ account_app/views.py
@@ -1,6 +1,7 @@
 from django.views.generic import CreateView
 from .forms import UserCreateForm
 from django.urls import reverse_lazy
+from django.contrib.auth.views import PasswordChangeView
 
 
 class SignUpView(CreateView):
@@ -11,3 +12,7 @@ class SignUpView(CreateView):
     form_class = UserCreateForm
     success_url = reverse_lazy('account_app:login')
     template_name = 'signup.html'
+
+
+class PassChangeView(PasswordChangeView):
+    success_url = reverse_lazy('account_app:password_change_done')
