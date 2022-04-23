a955797dcb77bc495fe06a5310a5e6a9dd571ee5
Mohammad Ali Gholampour <mohammad.aq1996@yahoo.com>
2022-04-24 00:05:32 +0430

    add password reset view

--- account_app/admin.py
+++ account_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+
+admin.site.register(User, UserAdmin)
+
+UserAdmin.fieldsets[1][1]['fields'] = ("first_name", "last_name", "email", "address", "phone_no")
